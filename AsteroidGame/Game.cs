80875ee76079fa7ec86799813405df4e9fb0454b
SouthAvalanche <evgeniy.sadomskiy@gmail.com>
2020-04-09 14:38:54 +0300

    1st lesson

---
+++ AsteroidGame/Game.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsteroidGame
+{
+    static class Game
+    {
+        private static BufferedGraphicsContext _context;
+        public static BufferedGraphics Buffer;
+
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+
+        static Game()
+        {
+
+        }
+
+        public static void Init(Form form)
+        {
+            //графическое устройство для вывода графики
+            Graphics g;
+            // доступ к графическому контексту буферу
+            _context = BufferedGraphicsManager.Current;
+            g = form.CreateGraphics();
+            //создаем объект (повернхность рисования) и связываем его с формой
+            //запоминаем размеры форм
+            Width = form.ClientSize.Width;
+            Height = form.ClientSize.Height;
+            //связываем буфер в памяти с графическим объектом, чтобы рисовать в буфере
+            Buffer = _context.Allocate(g, new Rectangle(0, 0, Width, Height));
+
+            Load();
+
+            Timer timer = new Timer { Interval = 100 };
+            timer.Start();
+            timer.Tick += Timer_Tick;
+
+        }
+
+        private static void Timer_Tick(object sender, EventArgs e)
+        {
+            Draw();
+            Update();
+        }
+
+        public static void Draw()
+        {
+            //Проверяем вывод графики
+            Buffer.Graphics.Clear(Color.Black);
+            Buffer.Graphics.DrawRectangle(Pens.White, new Rectangle(100, 100, 200, 200));
+            Buffer.Graphics.FillEllipse(Brushes.Wheat, new Rectangle(100, 100, 200, 200));
+            Buffer.Render();
+
+            foreach (BaseObject obj in _objs)
+                obj.Draw();
+            Buffer.Render();
+        }
+
+        public static void Update()
+        {
+            foreach (BaseObject obj in _objs)
+                obj.Update();
+        }
+
+        public static BaseObject[] _objs;
+        public static void Load()
+        {
+            _objs = new BaseObject[30];
+            for (int i = 0; i < _objs.Length; i++)
+                _objs[i] = new BaseObject(new Point(600, i * 20), new Point(15 - i, 15 - i), new Size(20, 20));
+        }
+
+    }
+}
---
+++ AsteroidGame/Properties/Game.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsteroidGame.Properties
+{
+    class Class1
+    {
+    }
+}
